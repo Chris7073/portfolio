8cc1652905455f4fe999ce96215da4cb74d4a674
Chris7073 <cicciarellachristian@gmail.com>
2025-05-30 16:36:36 +0200

    fixed hero

--- components/hero.vue
+++ components/hero.vue
@@ -1,5 +1,5 @@
 <script type="ts" setup>
-const { data: landingInfo, pending } = useFetch('/api/landing-settings')
+const { data: landingInfo } = useFetch('/api/landing-settings')
 watchEffect(() => {
   console.log('landingInfo:', landingInfo)
 })
@@ -20,16 +20,6 @@ const backgroundColor = computed(() => {
 );
 </script>
 <template>
-  <div v-if="pending" class="h-dvh bg-gray-200">
-    <div class="flex items-center justify-center h-full w-full">
-      <div class="flex flex-col gap-2">
-        <Skeleton class="w-50 bg-gray-300 h-5" />
-        <Skeleton class="w-50 bg-gray-300 h-2" />
-        <Skeleton class="w-50 bg-gray-400/80 h-10 mt-4" />
-      </div>
-    </div>
-  </div>
-  <div v-else>
     <div v-if="landingInfo?.hero" class="grid h-dvh " :style="backgroundColor">
       <div class="content-center text-center h-full" :style="'color:' + landingInfo.hero_bg[0].bg_text_color">
         <Icon name="uil:user" class="text-5xl" />
@@ -45,5 +35,4 @@ const backgroundColor = computed(() => {
 
       </div>
     </div>
-  </div>
 </template>
